46e81422a812a9b7fc1c3a62bda9f41cb0c3dc66
nmyers217 <nick.b.myers@protonmail.com>
2022-11-30 23:41:47 -0600

    Rewrite day 1 with ramda

--- day_01.ts
+++ day_01.ts
@@ -1,18 +1,21 @@
+import * as R from "https://deno.land/x/ramda/mod.ts";
+
 export async function solve() {
-  const sum = (arr: number[]) => arr.reduce((a, b) => a + b, 0);
+  const data = await Deno.readTextFile("./input/day_01.txt");
 
-  const data = await Deno.readTextFile('./input/day_01.txt');
-  const elves = data.split('\n\n').map((str) => str.split('\n').map(Number));
-  const sums = elves.map(sum);
+  const sums = R.pipe(
+    R.split("\n\n"),
+    R.map(R.pipe(R.split("\n"), R.map(Number), R.sum)),
+  )(data);
 
-  // Part one
-  let biggest = 0;
-  for (const s of sums) {
-    if (s > biggest) biggest = s;
-  }
-  console.log(biggest);
+  const partOne = R.reduce(R.max, 0, sums)
 
-  // Part two
-  const sortedSums = sums.sort((a, b) => (a < b ? -1 : 1)).reverse();
-  console.log(sum(sortedSums.slice(0, 3)));
+  const partTwo = R.pipe(
+    R.sort(R.comparator(R.gt)),
+    R.take(3),
+    R.sum,
+  )(sums);
+
+  console.log(partOne);
+  console.log(partTwo);
 }
