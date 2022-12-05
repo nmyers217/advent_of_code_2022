4d99b0da1b2c45a9a3885d0a91a1d46316a5f89a
nmyers217 <nick.b.myers@protonmail.com>
2022-12-05 15:56:24 -0600

    Day 5 fully functional

--- day_05.ts
+++ day_05.ts
@@ -1,29 +1,22 @@
 import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
-import { split } from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 
 const data = await Deno.readTextFile("./input/day_05.txt");
 
-const parseStacks = (str) => {
-  const transposedLines = R.pipe(
-    R.split("\n"),
-    R.reverse,
-    R.map(R.split("")),
-    R.transpose,
-  );
-
-  const transformLine = R.compose(
-    R.reject(R.startsWith([" "])),
-    R.map(R.tail),
-    R.map(R.reject(R.equals(" "))),
-  );
-
-  return R.transduce(
-    transformLine,
+const parseStacks = R.pipe(
+  R.split("\n"),
+  R.reverse,
+  R.map(R.split("")),
+  R.transpose,
+  R.transduce(
+    R.compose(
+      R.reject(R.startsWith([" "])),
+      R.map(R.tail),
+      R.map(R.reject(R.equals(" "))),
+    ),
     R.flip(R.append),
     [],
-    transposedLines(str),
-  );
-};
+  ),
+);
 
 const parseMoves = R.pipe(
   R.split("\n"),
@@ -31,29 +24,21 @@ const parseMoves = R.pipe(
   R.map(R.map(parseInt)),
 );
 
-const [stacksStr, movesStr] = split("\n\n", data);
+const moveReducer = (reverseFn) => (stacks, [amt, from, to]) => (
+  R.pipe(
+    R.nth(from - 1),
+    R.takeLast(amt),
+    reverseFn,
+    (cratesToMove) => R.adjust(to - 1, R.concat(R.__, cratesToMove), stacks),
+    R.adjust(from - 1, R.dropLast(amt)),
+  )(stacks)
+);
+
+const getLastStr = R.pipe(R.map(R.last), R.join(""));
+
+const [stacksStr, movesStr] = R.split("\n\n", data);
 const [stacks, moves] = [parseStacks(stacksStr), parseMoves(movesStr)];
 
-for (const [amt, from, to] of moves) {
-  for (let i = 0; i < amt; i++) {
-    if (stacks[from - 1].length > 0) {
-      stacks[to - 1].push(stacks[from - 1].pop());
-    }
-  }
-}
-
-const moreStacks = parseStacks(stacksStr);
-
-for (const [amt, from, to] of moves) {
-  let arr = [];
-  for (let i = 0; i < amt; i++) {
-    if (moreStacks[from - 1].length > 0) {
-      arr = arr.concat(moreStacks[from - 1].pop());
-    }
-  }
-  moreStacks[to - 1].push(...arr.reverse());
-}
-
 console.log("Day 5:");
-console.log(R.join("", R.map(R.last, stacks)));
-console.log(R.join("", R.map(R.last, moreStacks)));
+console.log(getLastStr(R.reduce(moveReducer(R.reverse), stacks, moves)));
+console.log(getLastStr(R.reduce(moveReducer(R.identity), stacks, moves)));
