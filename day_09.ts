a7ec298439f52c7892a7ab1da86bac9bdaa28639
nmyers217 <nick.b.myers@protonmail.com>
2022-12-09 14:39:26 -0600

    Day 9 slightly more functional

--- day_09.ts
+++ day_09.ts
@@ -1,48 +1,53 @@
 import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 
 const data = await Deno.readTextFile("./input/day_09.txt");
-const lines = R.split("\n", data);
 const dirs = { U: [-1, 0], D: [1, 0], L: [0, -1], R: [0, 1] };
 
-const solve = (ropeLen) => {
-  const rope = R.times(() => [0, 0], ropeLen);
-  const visited = { "0,0": true };
-
-  for (const line of lines) {
-    const [dir, mag] = R.pipe(
-      R.split(" "),
-      R.adjust(0, R.flip(R.prop)(dirs)),
-      R.adjust(1, parseInt),
-    )(line);
-
-    for (let n = 0; n < mag; n++) {
-      rope[0] = R.zipWith(R.add, rope[0], dir);
-
-      for (let i = 1; i < rope.length; i++) {
-        const [dy, dx] = R.zipWith(R.subtract, rope[i - 1], rope[i]);
-
-        if (dy < -1) {
-          rope[i][0]--;
-          if (dx !== 0) rope[i][1] += R.clamp(-1, 1, dx);
-        } else if (dy > 1) {
-          rope[i][0]++;
-          if (dx !== 0) rope[i][1] += R.clamp(-1, 1, dx);
-        } else if (dx < -1) {
-          rope[i][1]--;
-          if (dy !== 0) rope[i][0] += R.clamp(-1, 1, dy);
-        } else if (dx > 1) {
-          rope[i][1]++;
-          if (dy !== 0) rope[i][0] += R.clamp(-1, 1, dy);
-        }
-      }
-
-      visited[R.last(rope).toString()] = true;
-    }
+const moveTo = (vecA, vecB) => {
+  const result = R.clone(vecB);
+  const [dy, dx] = R.zipWith(R.subtract, vecA, vecB);
+  if (Math.abs(dy) > 1) {
+    result[0] += dy < 0 ? -1 : 1;
+    if (dx !== 0) result[1] += R.clamp(-1, 1, dx);
+  } else if (Math.abs(dx) > 1) {
+    result[1] += dx < 0 ? -1 : 1;
+    if (dy !== 0) result[0] += R.clamp(-1, 1, dy);
   }
-
-  return R.length(R.keys(visited));
+  return result;
 };
 
+const simulate = (ropeLen) => (data) => (
+  R.transduce(
+    R.map(R.pipe(
+      R.split(" "),
+      ([dirStr, nStr]) => [R.prop(dirStr, dirs), parseInt(nStr)],
+    )),
+    ({ rope, visited }, [dir, mag]) => (
+      R.reduce(
+        ({ rope, visited }, _) => {
+          rope[0] = R.zipWith(R.add, rope[0], dir);
+          for (let i = 1; i < rope.length; i++) {
+            rope[i] = moveTo(rope[i - 1], rope[i]);
+          }
+          visited[R.last(rope).toString()] = true;
+          return { rope, visited };
+        },
+        { rope, visited },
+        R.times(R.identity, mag),
+      )
+    ),
+    {
+      rope: R.times(() => [0, 0], ropeLen),
+      visited: { "0,0": true },
+    },
+    R.split("\n", data),
+  )
+);
+
+const solve = (ropeLen) => (
+  R.pipe(simulate(ropeLen), R.prop("visited"), R.keys, R.length)
+);
+
 console.log("Day 9:");
-console.log(solve(2));
-console.log(solve(10));
+console.log(solve(2)(data));
+console.log(solve(10)(data));
